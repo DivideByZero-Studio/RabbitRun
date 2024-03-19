703e1e43888c5c64b9690aea0f3baed0490f35c1
weeqn <gugobute@gmail.com>
2024-03-20 00:20:59 +0300

    jumps cooldown and capacity logic

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,19 +10,45 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpDuration; //in seconds
     [SerializeField, Min(1f)] private float jumpStepMultiplier;
     
-    public event Action Jumped;
-    public event Action Landed;
+    [SerializeField] private float jumpCooldown;
+    [SerializeField] private int jumpsCapacity;
+    [SerializeField] private float jumpReloadTime;
+    private bool _canJump = true;
+    private bool _isReloading;
+    private int _jumpsLeft;
 
     private Vector3 _jumpDirection { get; set; }
     
+    public event Action Jumped;
+    public event Action Landed;
+
+
+    private void Awake()
+    {
+        _jumpsLeft = jumpsCapacity;
+    }
+
     private void Jump()
     {
+        if (_canJump == false || _jumpsLeft <= 0) return;
+        
         StartCoroutine(JumpRoutine());
+    }
+
+    private void Update()
+    {
+        if (_jumpsLeft < jumpsCapacity && !_isReloading)
+        {
+            StartCoroutine(ReloadJump());
+        }
     }
 
     private IEnumerator JumpRoutine()
     {
         Jumped?.Invoke();
+        
+        _canJump = false;
+        _jumpsLeft--;
         
         playerVisuals.SetRotation(directionPointer.transform.rotation);
         _jumpDirection = directionPointer.transform.right;
@@ -36,8 +62,29 @@ public class PlayerMovement : MonoBehaviour
         }
         
         Landed?.Invoke();
+        
+        _canJump = true;
+
+        StartCoroutine(CooldownRoutine());
     }
 
+    private IEnumerator CooldownRoutine()
+    {
+        _canJump = false;
+        yield return new WaitForSeconds(jumpCooldown);
+        _canJump = true;
+    }
+
+    private IEnumerator ReloadJump()
+    {
+        _isReloading = true;
+
+        yield return new WaitForSeconds(jumpReloadTime);
+        _jumpsLeft += 1;
+
+        _isReloading = false;
+    }
+    
     private void OnCollisionEnter(Collision other)
     {
         if (TryGetComponent(out Obstacle obstacle))
